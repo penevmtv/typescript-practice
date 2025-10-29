d5b6e965947712f23f4a518618efb40f82fe6647
penevmtv <penevmtv@abv.bg>
2025-10-29 12:32:31 +0200

    create 02.ts

---
+++ Exercise/02.ts
@@ -0,0 +1,28 @@
+enum Direction {
+  east,
+  west,
+  north,
+  south
+}
+
+function move(direction: Direction): string {
+  const moved: 'up' | 'down' | 'right' | 'left';
+  
+  switch (direction) {
+    case Direction.east:
+      moved = 'right';
+      break;
+    case Direction.west:
+      moved = 'left';
+      break;
+    case Direction.north:
+      moved = 'up';
+      break;
+    case Direction.south:
+      moved = 'down';
+      break;
+  }
+  return `You moved ${moved}`;
+}
+
+console.log(move(Direction.east));
